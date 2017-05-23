d2c5e670760693fa8139cf17b597fc690fa40547
Jessica <jessica.zappa@mail.polimi.it>
2017-05-23 14:32:54 +0200

    Effect Constructor

--- src/main/java/it/polimi/ingsw/GC_23/Effects/Effect.java
+++ src/main/java/it/polimi/ingsw/GC_23/Effects/Effect.java
@@ -10,6 +10,34 @@ public class Effect {
     private NewPlayEffect newPlayEffect;
     private DiscountEffect discountEffect;
 
+    public Effect(CouncilPrivilegeEffect councilPrivilegeEffect, BenefitsEffect benefitsEffect, ImplicationEffect implicationEffect, NewPlayEffect newPlayEffect, DiscountEffect discountEffect) {
+        this.councilPrivilegeEffect = councilPrivilegeEffect;
+        this.benefitsEffect = benefitsEffect;
+        this.implicationEffect = implicationEffect;
+        this.newPlayEffect = newPlayEffect;
+        this.discountEffect = discountEffect;
+    }
+
+    public void setCouncilPrivilegeEffect(CouncilPrivilegeEffect councilPrivilegeEffect) {
+        this.councilPrivilegeEffect = councilPrivilegeEffect;
+    }
+
+    public void setBenefitsEffect(BenefitsEffect benefitsEffect) {
+        this.benefitsEffect = benefitsEffect;
+    }
+
+    public void setImplicationEffect(ImplicationEffect implicationEffect) {
+        this.implicationEffect = implicationEffect;
+    }
+
+    public void setNewPlayEffect(NewPlayEffect newPlayEffect) {
+        this.newPlayEffect = newPlayEffect;
+    }
+
+    public void setDiscountEffect(DiscountEffect discountEffect) {
+        this.discountEffect = discountEffect;
+    }
+
     public CouncilPrivilegeEffect getCouncilPrivilegeEffect() {
         return councilPrivilegeEffect;
     }
