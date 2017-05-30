257dcd4f7b42cc4f851fd761855d265f3535eb79
Jessica <jessica.zappa@mail.polimi.it>
2017-05-30 20:58:59 +0200

    fix finali

--- src/test/java/it/polimi/ingsw/GC_23/Cards/VentureCardTest.java
+++ src/test/java/it/polimi/ingsw/GC_23/Cards/VentureCardTest.java
@@ -15,30 +15,30 @@ import static org.junit.Assert.*;
  * Created by jesss on 28/05/17.
  */
 public class VentureCardTest {
-    @Test
-    public void checkTakeable() throws Exception {
-        Player player = new Player(RED, null);
-        ResourcesSet res1 = new ResourcesSet(1,2,3,4,5,6,7);
-        ResourcesSet res2 = new ResourcesSet(2,2,3,4,5,6,7);
-        ResourcesSet res3 = new ResourcesSet(4,2,3,4,0,6,7);
-        SingleCost cost1 = new SingleCost(res1);
-        SingleCost cost2 = new SingleCost(res3);
-        player.setResources(res2);
-        VentureCard card1 = new VentureCard(1, CardColor.YELLOW, null, null, null, cost1);
-        VentureCard card2 = new VentureCard(1, CardColor.GREEN, null, null, null, cost1);
-        VentureCard card3 = new VentureCard(1, CardColor.PURPLE, null, null, null, cost1);
-        VentureCard card4 = new VentureCard(1, CardColor.PURPLE, null, null, null,cost2);
-        CardOfPlayer cardOfPlayer = new CardOfPlayer();
-        player.setCardOfPlayer(cardOfPlayer);
-        player.getCardOfPlayer().setCard(card1);
-        player.getCardOfPlayer().setCard(card2);
-        player.getCardOfPlayer().setCard(card1);
-        assertEquals(true, card3.checkTakeable(player));
-        player.getCardOfPlayer().setCard(card2);
-        player.getCardOfPlayer().setCard(card1);
-        player.getCardOfPlayer().setCard(card2);
-        assertEquals(false, card3.checkTakeable(player));
-        assertEquals(false, card4.checkTakeable(player));
-    }
+  @Test
+  public void checkTakeable() throws Exception {
+    Player player = new Player(RED, null);
+    ResourcesSet res1 = new ResourcesSet(1,2,3,4,5,6,7);
+    ResourcesSet res2 = new ResourcesSet(2,2,3,4,5,6,7);
+    ResourcesSet res3 = new ResourcesSet(4,2,3,4,0,6,7);
+    SingleCost cost1 = new SingleCost(res1);
+    SingleCost cost2 = new SingleCost(res3);
+    player.setResources(res2);
+    VentureCard card1 = new VentureCard(1, CardColor.YELLOW, null, null, null, cost1);
+    VentureCard card2 = new VentureCard(1, CardColor.GREEN, null, null, null, cost1);
+    VentureCard card3 = new VentureCard(1, CardColor.PURPLE, null, null, null, cost1);
+    VentureCard card4 = new VentureCard(1, CardColor.PURPLE, null, null, null, cost2);
+    CardOfPlayer cardOfPlayer = new CardOfPlayer();
+    player.setCardOfPlayer(cardOfPlayer);
+    player.getCardOfPlayer().setCard(card1);
+    player.getCardOfPlayer().setCard(card2);
+    player.getCardOfPlayer().setCard(card1);
+    assertEquals(true, card3.checkTakeable(player));
+    player.getCardOfPlayer().setCard(card2);
+    player.getCardOfPlayer().setCard(card1);
+    player.getCardOfPlayer().setCard(card2);
+    assertEquals(false, card3.checkTakeable(player));
+    assertEquals(false, card4.checkTakeable(player));
+  }
 
 }
