5b2df01136c7f4cb3c04df2d64341cc5217a8711
Jessica <jessica.zappa@mail.polimi.it>
2017-06-01 22:59:09 +0200

    pulisco main

--- src/main/java/it/polimi/ingsw/GC_23/Main.java
+++ src/main/java/it/polimi/ingsw/GC_23/Main.java
@@ -28,34 +28,12 @@ import java.util.logging.Logger;
  * Created by Alessandro on 22/05/2017.
  */
 public class Main {
-
-    private ArrayList<Card> cardsList;
-    private int period;
-
     public static void main( String[] args )
     {
         ParseJson parseJson = new ParseJson();
         System.out.println( "Hello World!" );
         ResourcesSet set1 = new ResourcesSet(1,2,3,4,5,6,7);
         ResourcesSet set2 = new ResourcesSet(5,6,7,8,9,10,11);
-        BenefitsEffect benefitsEffect1= new BenefitsEffect(set1);
-        BenefitsEffect benefitsEffect2= new BenefitsEffect(set2);
-        ArrayList<BenefitsEffect> benefitsEffects = new ArrayList<BenefitsEffect>();
-        benefitsEffects.add(benefitsEffect1);
-        benefitsEffects.add(benefitsEffect2);
-        Player player = new Player(PlayerColor.RED, null);
-        SingleCost cost1 = new SingleCost(set1);
-        SingleCost cost2 = new SingleCost(set2);
-        ArrayList<SingleCost> costs = new ArrayList<>();
-        costs.add(cost1);
-        costs.add(cost2);
-        BuildingCard buildingCard = new BuildingCard(1,CardColor.YELLOW,"ciao", null
-        ,null,costs);
-        buildingCard.getCost(player);
-        ImplicationEffect implicationEffect = new ImplicationEffect(costs,benefitsEffects);
-        implicationEffect.chooseImplication(player);
-
-
     }
 
     public Main () {
